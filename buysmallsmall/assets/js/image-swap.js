0acef31af25648f39f4651b7631ec92531702738
yusuf <bwitlawalyusuf@gmail.com>
2023-03-31 04:22:19 +0100

    buysmallsmall folder removed

--- buysmallsmall/assets/js/image-swap.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-$('.the_thumbnails').click(function(){
-	"use strict";
-	var imgUrl = $(this).attr('id');
-	$('.prop-main-image').html('<img src="'+imgUrl+'" />');
-});
--- buysmallsmall/assets/js/property-summary.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-$('.summary-menu-item').click(function(){
-	"use strict";
-	var id = $(this).attr("id");
-	$('.summary-menu-item').removeClass('active-summary-menu');
-	$('#'+id).addClass('active-summary-menu');
-	$('.summary-container').hide(300);
-	$('.'+id).show(300);
-});
